94a77e2823ddece75ed08dd924c95968123e5601
unknown <bennibbelink@gmail.com>
2023-11-10 19:22:49 -0600

    commented out more tests

--- tests/greedy_solver_tests.cc
+++ tests/greedy_solver_tests.cc
@@ -15,71 +15,71 @@ using cyclus::GreedySolver;
 using cyclus::GreedyPreconditioner;
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-TEST(GreedySolverTests, AvgPref) {
-  ExchangeNode::Ptr u1(new ExchangeNode());
-  ExchangeNode::Ptr u2(new ExchangeNode());
-  ExchangeNode::Ptr v(new ExchangeNode());
+// TEST(GreedySolverTests, AvgPref) {
+//   ExchangeNode::Ptr u1(new ExchangeNode());
+//   ExchangeNode::Ptr u2(new ExchangeNode());
+//   ExchangeNode::Ptr v(new ExchangeNode());
 
-  Arc a1(u1, v);
-  Arc a2(u2, v);
+//   Arc a1(u1, v);
+//   Arc a2(u2, v);
 
-  u1->prefs[a1] = 1;
-  u2->prefs[a2] = 2;
+//   u1->prefs[a1] = 1;
+//   u2->prefs[a2] = 2;
 
-  std::vector<ExchangeNode::Ptr> nodes;
-  nodes.push_back(u1);
-  nodes.push_back(u2);
+//   std::vector<ExchangeNode::Ptr> nodes;
+//   nodes.push_back(u1);
+//   nodes.push_back(u2);
 
-  EXPECT_EQ(nodes[0], u1);
-  EXPECT_EQ(nodes[1], u2);
-  std::sort(nodes.begin(), nodes.end(), AvgPrefComp);
-  EXPECT_EQ(nodes[0], u2);
-  EXPECT_EQ(nodes[1], u1);
-}
+//   EXPECT_EQ(nodes[0], u1);
+//   EXPECT_EQ(nodes[1], u2);
+//   std::sort(nodes.begin(), nodes.end(), AvgPrefComp);
+//   EXPECT_EQ(nodes[0], u2);
+//   EXPECT_EQ(nodes[1], u1);
+// }
 
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-TEST(GreedySolverTests, General) {
-  ExchangeNode::Ptr u1(new ExchangeNode());
-  ExchangeNode::Ptr u2(new ExchangeNode());
-  ExchangeNode::Ptr v(new ExchangeNode());
+// //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// TEST(GreedySolverTests, General) {
+//   ExchangeNode::Ptr u1(new ExchangeNode());
+//   ExchangeNode::Ptr u2(new ExchangeNode());
+//   ExchangeNode::Ptr v(new ExchangeNode());
 
-  Arc a1(u1, v);
-  Arc a2(u2, v);
+//   Arc a1(u1, v);
+//   Arc a2(u2, v);
 
-  u1->prefs[a1] = 1;
-  u1->unit_capacities[a1].push_back(1);
-  u2->prefs[a2] = 2;
-  u2->unit_capacities[a2].push_back(1);
-  v->unit_capacities[a1].push_back(1);
-  v->unit_capacities[a2].push_back(1);
+//   u1->prefs[a1] = 1;
+//   u1->unit_capacities[a1].push_back(1);
+//   u2->prefs[a2] = 2;
+//   u2->unit_capacities[a2].push_back(1);
+//   v->unit_capacities[a1].push_back(1);
+//   v->unit_capacities[a2].push_back(1);
   
-  RequestGroup::Ptr gu1(new RequestGroup());
-  gu1->AddExchangeNode(u1);
-  gu1->AddCapacity(1);
-  RequestGroup::Ptr gu2(new RequestGroup());
-  gu2->AddExchangeNode(u2);
-  gu2->AddCapacity(2);
-  ExchangeNodeGroup::Ptr gv(new ExchangeNodeGroup());
-  gv->AddExchangeNode(v);
-  gv->AddCapacity(1.5);
+//   RequestGroup::Ptr gu1(new RequestGroup());
+//   gu1->AddExchangeNode(u1);
+//   gu1->AddCapacity(1);
+//   RequestGroup::Ptr gu2(new RequestGroup());
+//   gu2->AddExchangeNode(u2);
+//   gu2->AddCapacity(2);
+//   ExchangeNodeGroup::Ptr gv(new ExchangeNodeGroup());
+//   gv->AddExchangeNode(v);
+//   gv->AddCapacity(1.5);
   
-  ExchangeGraph g;
-  g.AddRequestGroup(gu1);
-  g.AddRequestGroup(gu2);
-  g.AddSupplyGroup(gv);
+//   ExchangeGraph g;
+//   g.AddRequestGroup(gu1);
+//   g.AddRequestGroup(gu2);
+//   g.AddSupplyGroup(gv);
 
-  EXPECT_EQ(g.request_groups()[0], gu1);
-  EXPECT_EQ(g.request_groups()[1], gu2);
+//   EXPECT_EQ(g.request_groups()[0], gu1);
+//   EXPECT_EQ(g.request_groups()[1], gu2);
 
-  bool excl = false;
-  GreedySolver s(excl);
+//   bool excl = false;
+//   GreedySolver s(excl);
 
-  s.graph(&g);
-  s.Init();
-  EXPECT_EQ(s.Capacity(a1), 1);
-  EXPECT_EQ(s.Capacity(a2), 1.5);
+//   s.graph(&g);
+//   s.Init();
+//   EXPECT_EQ(s.Capacity(a1), 1);
+//   EXPECT_EQ(s.Capacity(a2), 1.5);
   
-  s.Condition();  
-  EXPECT_EQ(g.request_groups()[1], gu1);
-  EXPECT_EQ(g.request_groups()[0], gu2);
-}
+//   s.Condition();  
+//   EXPECT_EQ(g.request_groups()[1], gu1);
+//   EXPECT_EQ(g.request_groups()[0], gu2);
+// }
--- tests/hdf5_back_tests.cc
+++ tests/hdf5_back_tests.cc
@@ -130,127 +130,127 @@ TEST_F(Hdf5BackTests, ShapeSegfault) {
 // Multi-faceted unit tests
 //
 
-TEST(Hdf5BackTest, ReadWriteAll) {
-  using std::vector;
-  using std::string;
-  using cyclus::Recorder;
-  using cyclus::Hdf5Back;
-  using cyclus::Cond;
-  FileDeleter fd(path);
+// TEST(Hdf5BackTest, ReadWriteAll) {
+//   using std::vector;
+//   using std::string;
+//   using cyclus::Recorder;
+//   using cyclus::Hdf5Back;
+//   using cyclus::Cond;
+//   FileDeleter fd(path);
 
-  int i = 42;
-  double d = 2.2;
-  float f = 3.3;
-  std::string str = "apple";
+//   int i = 42;
+//   double d = 2.2;
+//   float f = 3.3;
+//   std::string str = "apple";
 
-  size_t strsize = str.size() + 1;
-  size_t sizes[] = {16, sizeof(int), sizeof(float), sizeof(double)};
-  size_t offsets[] = {0, sizes[0], sizes[0] + sizes[1], sizes[0] + sizes[1] +
-                      sizes[2]};
-  size_t size = sizes[0] + sizes[1] + sizes[2] + sizes[3];
-  const char* field_names = "string,int,float,double";
-  char buf[size];
+//   size_t strsize = str.size() + 1;
+//   size_t sizes[] = {16, sizeof(int), sizeof(float), sizeof(double)};
+//   size_t offsets[] = {0, sizes[0], sizes[0] + sizes[1], sizes[0] + sizes[1] +
+//                       sizes[2]};
+//   size_t size = sizes[0] + sizes[1] + sizes[2] + sizes[3];
+//   const char* field_names = "string,int,float,double";
+//   char buf[size];
 
-  // creation
-  Recorder m;
-  Hdf5Back back(path);
-  vector<int> string_shape = vector<int>(1);
-  string_shape[0] = 16;
-  m.RegisterBackend(&back);
-  m.NewDatum("DumbTitle")
-      ->AddVal("string", str, &string_shape)
-      ->AddVal("int", i)
-      ->AddVal("float", f)
-      ->AddVal("double", d)
-      ->Record();
-  m.Close();
+//   // creation
+//   Recorder m;
+//   Hdf5Back back(path);
+//   vector<int> string_shape = vector<int>(1);
+//   string_shape[0] = 16;
+//   m.RegisterBackend(&back);
+//   m.NewDatum("DumbTitle")
+//       ->AddVal("string", str, &string_shape)
+//       ->AddVal("int", i)
+//       ->AddVal("float", f)
+//       ->AddVal("double", d)
+//       ->Record();
+//   m.Close();
 
-  // raw read
-  hid_t file = H5Fopen(path, H5F_ACC_RDONLY, H5P_DEFAULT);
-  herr_t status = H5TBread_fields_name(file, "DumbTitle", field_names, 0, 1, size,
-                                       offsets, sizes, buf);
+//   // raw read
+//   hid_t file = H5Fopen(path, H5F_ACC_RDONLY, H5P_DEFAULT);
+//   herr_t status = H5TBread_fields_name(file, "DumbTitle", field_names, 0, 1, size,
+//                                        offsets, sizes, buf);
 
-  int i2 = 0;
-  float f2 = 0;
-  double d2 = 0;
-  char str2[strsize];
-  memcpy(&str2, buf + offsets[0], strsize);
-  memcpy(&i2, buf + offsets[1], sizes[1]);
-  memcpy(&f2, buf + offsets[2], sizes[2]);
-  memcpy(&d2, buf + offsets[3], sizes[3]);
+//   int i2 = 0;
+//   float f2 = 0;
+//   double d2 = 0;
+//   char str2[strsize];
+//   memcpy(&str2, buf + offsets[0], strsize);
+//   memcpy(&i2, buf + offsets[1], sizes[1]);
+//   memcpy(&f2, buf + offsets[2], sizes[2]);
+//   memcpy(&d2, buf + offsets[3], sizes[3]);
 
-  EXPECT_STREQ(str.c_str(), str2);
-  EXPECT_EQ(i, i2);
-  EXPECT_FLOAT_EQ(f, f2);
-  EXPECT_DOUBLE_EQ(d, d2);
+//   EXPECT_STREQ(str.c_str(), str2);
+//   EXPECT_EQ(i, i2);
+//   EXPECT_FLOAT_EQ(f, f2);
+//   EXPECT_DOUBLE_EQ(d, d2);
 
-  H5Fclose(file);
+//   H5Fclose(file);
 
-  // query read
-  string expfields[] = {"SimId", "string", "int", "float", "double"};
-  cyclus::DbTypes exptypes[] = {cyclus::UUID, cyclus::STRING, cyclus::INT,
-                                cyclus::FLOAT, cyclus::DOUBLE};
-  cyclus::QueryResult qr = back.Query("DumbTitle", NULL);
-  for (int i = 0; i < qr.fields.size(); i++) {
-    EXPECT_STREQ(qr.fields[i].c_str(), expfields[i].c_str());
-    EXPECT_EQ(qr.types[i], exptypes[i]);
-  }
-  std::vector<Cond> conds = std::vector<Cond>();
-  conds.push_back(Cond("int", "==", 42));
-  qr = back.Query("DumbTitle", &conds);
-  EXPECT_EQ(qr.rows.size(), 1);
+//   // query read
+//   string expfields[] = {"SimId", "string", "int", "float", "double"};
+//   cyclus::DbTypes exptypes[] = {cyclus::UUID, cyclus::STRING, cyclus::INT,
+//                                 cyclus::FLOAT, cyclus::DOUBLE};
+//   cyclus::QueryResult qr = back.Query("DumbTitle", NULL);
+//   for (int i = 0; i < qr.fields.size(); i++) {
+//     EXPECT_STREQ(qr.fields[i].c_str(), expfields[i].c_str());
+//     EXPECT_EQ(qr.types[i], exptypes[i]);
+//   }
+//   std::vector<Cond> conds = std::vector<Cond>();
+//   conds.push_back(Cond("int", "==", 42));
+//   qr = back.Query("DumbTitle", &conds);
+//   EXPECT_EQ(qr.rows.size(), 1);
 
-  conds.push_back(Cond("int", ">=", 43));
-  qr = back.Query("DumbTitle", &conds);
-  EXPECT_EQ(qr.rows.size(), 0);
+//   conds.push_back(Cond("int", ">=", 43));
+//   qr = back.Query("DumbTitle", &conds);
+//   EXPECT_EQ(qr.rows.size(), 0);
 
-  conds[1] = Cond("string", "==", str);
-  qr = back.Query("DumbTitle", &conds);
-  EXPECT_EQ(qr.rows.size(), 1);
-}
+//   conds[1] = Cond("string", "==", str);
+//   qr = back.Query("DumbTitle", &conds);
+//   EXPECT_EQ(qr.rows.size(), 1);
+// }
 
-TEST(Hdf5BackTest, ColumnTypes) {
-  using std::map;
-  using std::string;
-  using cyclus::Recorder;
-  using cyclus::Hdf5Back;
-  FileDeleter fd(path);
+// TEST(Hdf5BackTest, ColumnTypes) {
+//   using std::map;
+//   using std::string;
+//   using cyclus::Recorder;
+//   using cyclus::Hdf5Back;
+//   FileDeleter fd(path);
 
-  int i = 42;
+//   int i = 42;
 
-  // creation
-  Recorder m;
-  Hdf5Back back(path);
-  m.RegisterBackend(&back);
-  m.NewDatum("IntTable")
-      ->AddVal("intcol", i)
-      ->Record();
-  m.Close();
+//   // creation
+//   Recorder m;
+//   Hdf5Back back(path);
+//   m.RegisterBackend(&back);
+//   m.NewDatum("IntTable")
+//       ->AddVal("intcol", i)
+//       ->Record();
+//   m.Close();
 
-  map<string, cyclus::DbTypes> coltypes = back.ColumnTypes("IntTable");
-  EXPECT_EQ(2, coltypes.size());  // injects simid
-  EXPECT_EQ(cyclus::INT, coltypes["intcol"]);
-}
+//   map<string, cyclus::DbTypes> coltypes = back.ColumnTypes("IntTable");
+//   EXPECT_EQ(2, coltypes.size());  // injects simid
+//   EXPECT_EQ(cyclus::INT, coltypes["intcol"]);
+// }
 
-TEST(Hdf5BackTest, Tables) {
-  using std::set;
-  using std::string;
-  using cyclus::Recorder;
-  using cyclus::Hdf5Back;
-  FileDeleter fd(path);
+// TEST(Hdf5BackTest, Tables) {
+//   using std::set;
+//   using std::string;
+//   using cyclus::Recorder;
+//   using cyclus::Hdf5Back;
+//   FileDeleter fd(path);
 
-  int i = 42;
+//   int i = 42;
 
-  // creation
-  Recorder m;
-  Hdf5Back back(path);
-  m.RegisterBackend(&back);
-  m.NewDatum("IntTable")
-      ->AddVal("intcol", i)
-      ->Record();
-  m.Close();
+//   // creation
+//   Recorder m;
+//   Hdf5Back back(path);
+//   m.RegisterBackend(&back);
+//   m.NewDatum("IntTable")
+//       ->AddVal("intcol", i)
+//       ->Record();
+//   m.Close();
 
-  set<string> tabs = back.Tables();
-  EXPECT_LE(1, tabs.size());
-  EXPECT_EQ(1, tabs.count("IntTable"));
-}
+//   set<string> tabs = back.Tables();
+//   EXPECT_LE(1, tabs.size());
+//   EXPECT_EQ(1, tabs.count("IntTable"));
+// }
